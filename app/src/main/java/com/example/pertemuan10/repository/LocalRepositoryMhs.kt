04b17e4b130e2ad336718836a2dd643113f541c3
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-12-17 15:27:14 +0700

    get all mahasiswa

--- app/src/main/java/com/example/pertemuan10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,16 @@ package com.example.pertemuan10.repository
 
 import com.example.pertemuan10.data.dao.MahasiswaDao
 import com.example.pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+    //get all mahasiswa
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
     }
 }
